8059d083d71dc0fdd6c106628f18a067a768abee
Nikita Gorkavchuk <55953545+Exage@users.noreply.github.com>
2024-05-17 10:21:08 +0300

    Add files via upload

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -7,7 +7,7 @@ button4 = types.KeyboardButton(text='Покажи лису')
 button5 = types.KeyboardButton(text='/prof')
 
 keyboard1 = [
-    [button1, button2, button3],
+    [button2, button3],
     [button4],
     [button5],
 ]
